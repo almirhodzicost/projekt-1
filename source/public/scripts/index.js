b8121196fccdd8e3f0d08e92bad738d5c641ece7
almirh <24458972+almirhodzic@users.noreply.github.com>
2023-05-24 14:39:19 +0200

    Some stying ishues

--- source/public/scripts/index.js
+++ source/public/scripts/index.js
@@ -4,11 +4,11 @@
     ----------------------------------
     */
     const ToDos = [
-        { id:1, duedate:"2023-05-20", description:"schwimmen", priority: 1, status:"done" },
-        { id:2, duedate:"2023-05-19", description:"einkaufen", priority: 2, status:"done" },
-        { id:3, duedate:"2023-05-15", description:"lernen", priority: 3, status:"done" },
-        { id:4, duedate:"2023-05-13", description:"singen", priority: 4, status:"done" },
-        { id:5, duedate:"2023-05-13", description:"spazieren", priority: 4, status:"done" }
+        { id:1, duedate:"2023-05-20", title:"title1", description:"schwimmen", priority: 1, status:"done" },
+        { id:2, duedate:"2023-05-19", title:"title2", description:"einkaufen", priority: 2, status:"done" },
+        { id:3, duedate:"2023-05-15", title:"title3", description:"lernen", priority: 3, status:"done" },
+        { id:4, duedate:"2023-05-13", title:"title4", description:"singen", priority: 4, status:"done" },
+        { id:5, duedate:"2023-05-13", title:"title5", description:"spazieren", priority: 4, status:"done" }
     ];
 
 
@@ -18,11 +18,3 @@
      */
     const themeSwitcher = document.getElementById("theme-toggler");
 
-
-    /*
-    Theme Toggle Event Listener
-    ----------------------------------
-     */
-    themeSwitcher.addEventListener("click", () => {
-        document.body.classList.toggle("dark-theme");
-    });
