e9be34ff4dae8c0f0195967b060b5054c80af7fe
almirh <24458972+almirhodzic@users.noreply.github.com>
2023-07-12 10:30:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-/*import express from 'express';
-
-const app = express();
-const port = 3000;
-
-app.use(express.static('public'));
-
-app.listen(port, () => {
-    console.log(`App is listening at http://localhost:${port}`);
-});*/
-
 import dotenv from "dotenv";
 
 // load config-file
